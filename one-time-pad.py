1a425f95312b8242209300bddc2eb5eb3a656dde
Pablo Viana <via16091@uvg.edu.gt>
2021-03-02 16:02:19 -0600

    encriptacion complete

--- one-time-pad.py
+++ one-time-pad.py
@@ -1,36 +1,52 @@
+"""
+Universidad del Valle de Guatemala
+Seguridad en sistemas de computación
+Catedrático: Melinton Navas
+Pablo Viana - 16091
+Dieter de Wit - 
+
+Programa para simular una comunicación encriptada por el método one time pad, que acepta cadenas de catacteres menores a 27, sin espacios.
+
+Código basado en implementación anterior de albohlabs https://github.com/albohlabs/one-time-pad
+"""
+
 import string
 import random
 import sys
 
-abc = string.ascii_lowercase
-one_time_pad = list(abc)
-# random.shuffle(one_time_pad)
+#obtenemos los caracteres del abecedario en minuscula 
+abecedario = string.ascii_lowercase
+one_time_pad = list(abecedario)
 
+#Código abstraido del encontrado en usuario albohlabs https://github.com/albohlabs/one-time-pad
 help = """Synopsis: otp.py -e|-d
 -e encrypt
 -d decrypt """
 
+def encriptar(mensaje, llave):
+    texto_encriptado = ''
+    for index, char in enumerate(mensaje):
+        #buscamos el indice del primer caracter del mensaje en el abecedario
+        char_index = abecedario.index(char)
+        #Buscamos el indice del caracter encontrado en el arreglo llave[posicion indice en que va el ciclo]
+        llave_index = one_time_pad.index(llave[index])
 
-def encrypt(msg, key):
-    ciphertext = ''
-    for idx, char in enumerate(msg):
-        charIdx = abc.index(char)
-        keyIdx = one_time_pad.index(key[idx])
+        #realizamos el cifrado
+        encriptado = (char_index + llave_index) % len(one_time_pad)
+        #armamos la palabra encriptada
+        texto_encriptado += abecedario[encriptado]
 
-        cipher = (keyIdx + charIdx) % len(one_time_pad)
-        ciphertext += abc[cipher]
-
-    return ciphertext
+    return texto_encriptado
 
 def decrypt(ciphertext, key):
     if ciphertext == '' or key == '':
         return ''
 
-    charIdx = abc.index(ciphertext[0])
+    charIdx = abecedario.index(ciphertext[0])
     keyIdx = one_time_pad.index(key[0])
 
     cipher = (charIdx - keyIdx) % len(one_time_pad)
-    char = abc[cipher]
+    char = abecedario[cipher]
 
     return char + decrypt(ciphertext[1:], key[1:])
 
@@ -40,10 +56,16 @@ if __name__ == '__main__':
         print(help)
         exit(0)
 
-    key = input("Key: ")
-    msg = input("Message: ")
+    mensaje = input("mensaje: ")
 
     if sys.argv[1] == availableOpt[1]:
-        print(encrypt(msg, key))
+        dict_llave = list(string.ascii_lowercase)
+        random.shuffle(dict_llave)
+        llave = dict_llave[:len(mensaje)]
+        llave_txt = ''.join([str(elem) for elem in llave]) 
+        text_file = open("llave.txt", "w")
+        text_file.write(llave_txt)
+        text_file.close()
+        print(encriptar(mensaje, llave))
     elif sys.argv[1] == availableOpt[0]:
-        print(decrypt(msg, key))
+        print(decrypt(mensaje, llave))
